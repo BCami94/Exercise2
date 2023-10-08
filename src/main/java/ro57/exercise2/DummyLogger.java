2329d142da87e05987988bb4273fed8c138416af
Camelia B <camelia.bordencea17@gmail.com>
2023-10-08 09:51:40 +0300

    Initial commit

---
+++ src/main/java/ro57/exercise2/DummyLogger.java
@@ -0,0 +1,18 @@
+package ro57.exercise2;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class DummyLogger implements CommandLineRunner {
+  public void sayHello(String className) {
+    log.info("hello from DummyLogger:" + className );
+  }
+
+  @Override
+  public void run(String... args) throws Exception {
+    System.out.println("DummyLogger bean was created");
+  }
+}
